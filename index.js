731b581b6caa08563eb5ed2d3daf4b803c2e2c04
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-04-02 08:01:52 -0400

    gets all data from fortune database and shows on local

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const express = require('express');
-const PORT = 3000;
+const port = 4000;
+const Fortune = require('./models/fortune');
 
 const app = express();
+
+app.get('/fortune',async (req, res) => {
+    console.log('Sending the home page I guess');
+    res.send(await Fortune.getAll())
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
