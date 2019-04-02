540d4f91263f9477f19875d0f13c1e9583c019ea
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-04-02 07:06:34 -0400

    added testing, sql fortune database

---
+++ schema.sql
@@ -0,0 +1,5 @@
+create table fortune (
+    id serial primary key,
+    written_fortune varchar(200),
+    lucky_numbers varchar(100)
+);
---
+++ seed.sql
@@ -0,0 +1,9 @@
+insert into fortune
+    (written_fortune, lucky_numbers)
+values
+    ('You will have a great week','7,3,1,5,3'),
+    ('You will have a great week','4,1,2,5,3'),
+    ('You will have a great week','6,3,8,7,3'),
+    ('You will have a great week','9,3,3,5,3')
+;
+
